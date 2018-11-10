4b60637b64bd603114a5dbc58f92dbfd29038f59
Simon Su <xmansimon@live.com>
2018-11-09 21:05:35 -0500

    Some add on

--- resources/views/names/generator.blade.php
+++ resources/views/names/generator.blade.php
@@ -13,25 +13,26 @@
         <fieldset>
 
             <span>Enter your birth year:</span>
-            <label><input type='text' name='year' id = 'year' value="{{ $year }}"></label>
+            <label><input type='text' name='year' id = 'year' value='{{old('year')}}'></label>
+
             @include('modules.field-error', ['field' => 'year'])
             <br>
 
             <span>Personality </span>
             <select name="personality" id = 'personality' value ='{{ ($personality) }}' single>
-                <option value=1 {{($personality == "1")? 'selected' : ''}}>ambitious</option>
-                <option value=2 {{($personality == "2")? 'selected' : ''}}>compassionate</option>
-                <option value=3 {{($personality == "3")? 'selected' : ''}}>diligent</option>
-                <option value=4 {{($personality == "4")? 'selected' : ''}}>persistent</option>
-                <option value=5 {{($personality == "5")? 'selected' : ''}}>unassuming</option>
-                <option value=6 {{($personality == "6")? 'selected' : ''}}>reliable</option>
+                <option value=1 {{(old('personality') == "1")? 'selected' : ''}}>ambitious</option>
+                <option value=2 {{(old('personality') == "2")? 'selected' : ''}}>compassionate</option>
+                <option value=3 {{(old('personality') == "3")? 'selected' : ''}}>diligent</option>
+                <option value=4 {{(old('personality') == "4")? 'selected' : ''}}>persistent</option>
+                <option value=5 {{(old('personality') == "5")? 'selected' : ''}}>unassuming</option>
+                <option value=6 {{(old('personality') == "6")? 'selected' : ''}}>reliable</option>
             </select>
             @include('modules.field-error', ['field' => 'personality'])
             <br>
 
             <span>Gender:</span>
-            <label class="radio-inline"><input type='radio'name='sex' value="male"  {{($sex == "male")? 'checked' : ''}} > male</label>
-            <label class="radio-inline"><input type='radio'name='sex' value="female" {{($sex == "female")? 'checked' : ''}}> female</label>
+            <label class="radio-inline"><input type='radio'name='sex' value='male'  {{(old('sex') == "male")? 'checked' : ''}} > male</label>
+            <label class="radio-inline"><input type='radio'name='sex' value='female' {{(old('sex') == "female")? 'checked' : ''}}> female</label>
             @include('modules.field-error', ['field' => 'sex'])
 
 
@@ -44,7 +45,7 @@
 
 
     @if(!empty($searchResults))
-        <h2>Suggested name: </h2>
+        <h2>Suggested name based on your inputs: <br> Birth year: <em>{{ $year }}</em> ; Gender: <em>{{ $sex }} and your personality</em></h2>
 
         @foreach($searchResults as $name)
             <div class='book'>
